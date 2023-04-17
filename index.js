172b21fd7a8c29ac6fa606ee3430b3ece0761cf9
Jonas <j.froe@gmx.at>
2023-04-17 09:15:40 +0200

    relative path to servers

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-const defaultServerPath = 'default/index.js';
+const defaultServerPath = './default/index.js';
 const defaultServerProcess = spawn('node', [defaultServerPath], { env: { PORT: 4000 } });
 
 defaultServerProcess.on('exit', (code) => {
   console.log(`Der erste Server wurde mit dem Code ${code} beendet!`);
 });
 
-const roomServerPath = 'room/index.js';
+const roomServerPath = './room/index.js';
 const roomServerProcess = spawn('node', [roomServerPath], { env: { PORT: 5000 } });
 
 roomServerProcess.on('exit', (code) => {
