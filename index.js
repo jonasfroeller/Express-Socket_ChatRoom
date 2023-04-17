1cf42e77044d08ea1f3ff5b54c1783472c753edc
Jonas <j.froe@gmx.at>
2023-04-17 10:29:35 +0200

    nodemon

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+const path = require('path');
 const { spawn } = require('child_process');
 const express = require('express');
 
 const app = express();
 app.use(express.static('public'));
 
-const defaultServerPath = './default/index.js';
+const defaultServerPath = path.join(__dirname, 'default/index.js');
 const defaultServerProcess = spawn('node', [defaultServerPath], { env: { PORT: 4000 } });
 
 defaultServerProcess.on('exit', (code) => {
   console.log(`Der erste Server wurde mit dem Code ${code} beendet!`);
 });
 
-const roomServerPath = './room/index.js';
+const roomServerPath = path.join(__dirname, 'room/index.js');
 const roomServerProcess = spawn('node', [roomServerPath], { env: { PORT: 5000 } });
 
 roomServerProcess.on('exit', (code) => {
